2f796cfbeb8f19e2cb65b503219bd303f70e0f68
Jeevan <jeevan2774842@gmail.com>
2022-01-07 14:29:34 +0000

    solution

---
+++ index.js
@@ -0,0 +1,4 @@
+const { add, sub } = require("./calculator");
+
+console.log(add(5, 5));
+console.log(sub(10, 7));
---
+++ index.mjs
@@ -0,0 +1,4 @@
+import { add, sub } from "./calculator.mjs";
+
+console.log(add(25, 75));
+console.log(sub(25, 75));
